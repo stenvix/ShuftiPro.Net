d78bb346bc9e7b653321a844e1c9f6456422f865
Denys Trishchuk <denys.trishchuk@everymatrix.com>
2023-01-23 11:34:25 +0200

    add new address type

--- src/ShuftiPro/Enums/ShuftiProAddressType.cs
+++ src/ShuftiPro/Enums/ShuftiProAddressType.cs
@@ -7,6 +7,9 @@ namespace ShuftiPro.Enums
     [JsonConverter(typeof(StringEnumConverter))]
     public enum ShuftiProAddressType
     {
+        [EnumMember(Value = "")]
+        Undefined,
+
         [EnumMember(Value = "id_card")]
         IdCard,
 
